ae680c1ec03be538472d0d5e1ed9639c2edb1a6e
James Muscat <james.muscat@alces-software.com>
2017-07-12 18:03:04 +0100

    Be smarter: allow lookup by username or UUID.

--- app/resources/v1/user_resource.rb
+++ app/resources/v1/user_resource.rb
@@ -2,10 +2,20 @@ module V1
   class UserResource < JSONAPI::Resource
     attributes :name
 
-    # Allow accessing user by username not ID
-    primary_key :name
+    has_many :gridware
+
     key_type :string
 
-    has_many :gridware
+    UUID_REGEX = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+
+    def self.find_by_key(key, options = {})
+      if UUID_REGEX.match?(key)
+        super
+      else
+        model = User.find_by_name(key)
+        fail JSONAPI::Exceptions::RecordNotFound.new(key) if model.nil?
+        resource_for_model(model).new(model, options[:context])
+      end
+    end
   end
 end
