94b23b5987d03a1bb0107648489137b6ff7e8d4b
James Muscat <james.muscat@alces-software.com>
2017-07-17 13:12:19 +0100

    Make created/updated-at dates accessible.

--- app/resources/v1/gridware_resource.rb
+++ app/resources/v1/gridware_resource.rb
@@ -3,6 +3,7 @@ module V1
     model_name 'GridwarePackage'
 
     attributes :name, :group, :version, :summary, :url, :description, :package_type
+    attributes :updated_at, :created_at
 
     has_one :user
   end
