933687584a6ef2dd1dd8d2445f895e3289d1d294
James Muscat <james.muscat@alces-software.com>
2017-07-21 15:55:01 +0100

    Use OptionalLimit paginator for article resource too.

--- app/resources/v1/article_resource.rb
+++ app/resources/v1/article_resource.rb
@@ -1,8 +1,12 @@
 module V1
   class ArticleResource < JSONAPI::Resource
+
+    paginator :optional_limit
+
     attributes :title, :text
     attributes :updated_at, :created_at
 
     has_one :user
+
   end
 end
