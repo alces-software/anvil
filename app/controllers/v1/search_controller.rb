f509f8bb6517c3cd3a32f7489a542ef1a8c1a618
James Muscat <james.muscat@alces-software.com>
2017-07-31 13:18:46 +0100

    Rough & ready implementation of an omni-search.

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -1,7 +1,50 @@
 module V1
   class SearchController < ApplicationController
     def search
-      render :json => { :query => params[:q] }
+
+      query = params[:q]
+      gridware_packages = GridwarePackage.where('name LIKE ?', "%#{params[:q]}%").accessible_by(current_ability)
+      customizers = Customizer.where('name LIKE ?', "%#{params[:q]}%").accessible_by(current_ability)
+      articles = Article.where('title LIKE ?', "%#{params[:q]}%").accessible_by(current_ability)
+      users = User.where('name LIKE ?', "%#{params[:q]}%").accessible_by(current_ability)
+
+      result = {
+          query: query,
+          gridware: {},
+          customizers: {},
+          articles: {},
+          users: {}
+      }
+
+      gridware_packages.each do |g|
+        result[:gridware][g.id] = {
+            name: g.name,
+            version: g.version,
+            username: g.user.name
+        }
+      end
+
+      customizers.each do |c|
+        result[:customizers][c.id] = {
+            name: c.name,
+            username: c.user.name
+        }
+      end
+
+      articles.each do |a|
+        result[:articles][a.id] = {
+            title: a.title,
+            username: a.user.name
+        }
+      end
+
+      users.each do |u|
+        result[:users][u.id] = {
+            name: u.name
+        }
+      end
+
+      render :json => result
     end
   end
 end
