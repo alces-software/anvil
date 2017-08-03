ed50ed9c7cca113e1f5722ffcab5690a1c3450dc
James Muscat <james.muscat@alces-software.com>
2017-08-03 17:44:16 +0100

    Force all tags to be lowercase, with digits and underscore also allowed.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ApplicationRecord
 
+  validates :id, format: { with: /\A[a-z_0-9]+\z/ }
+
   has_many :taggings
 
   has_many :articles, through: :taggings, source: :taggable, source_type: 'Article'
@@ -7,7 +9,7 @@ class Tag < ApplicationRecord
   has_many :gridware_packages, through: :taggings, source: :taggable, source_type: 'GridwarePackage'
 
   def self.get_or_create(tag)
-    Tag.find_by_id(tag) || Tag.create(id: tag)
+    Tag.find_by_id(tag.downcase) || Tag.create(id: tag.downcase)
   end
 
 end
