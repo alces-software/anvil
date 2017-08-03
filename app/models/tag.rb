05ab21600354d42b7f2bb72c3205cfa6922f9fda
James Muscat <james.muscat@alces-software.com>
2017-08-03 17:05:45 +0100

    Utility method to create a tag if required.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,4 +6,8 @@ class Tag < ApplicationRecord
   has_many :customizers, through: :taggings, source: :taggable, source_type: 'Customizer'
   has_many :gridware_packages, through: :taggings, source: :taggable, source_type: 'GridwarePackage'
 
+  def self.get_or_create(tag)
+    Tag.find_by_id(tag) || Tag.create(id: tag)
+  end
+
 end
