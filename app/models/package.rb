cdf780c8eb925b0f5d1c1545bac9354a3c0b5e4d
James Muscat <james.muscat@alces-software.com>
2018-01-18 12:59:13 +0000

    Validate that specified dependencies are actual packages.

--- app/models/package.rb
+++ app/models/package.rb
@@ -22,8 +22,48 @@ class Package < ApplicationRecord
   validates :package_url,
             presence: true
 
+  validate :validate_dependencies
+
   def username
     # Convenience method to embed username in package resource without including everything to do with user
     user.name
+  end
+
+  def self.from_package_path(path)
+    package_props = split_package_path(path)
+    user = User.find_by_name(package_props[:user])
+    candidates = Package.where(user: user, name: package_props[:package])
+
+    if package_props[:version]
+      candidates.find_by_version(package_props[:version])
+    else
+      candidates.order(version: :desc).first
+    end
+  end
+
+  private
+
+  def self.split_package_path(path)
+    match = /(?<user>[^\/]+)\/(?<package>[^\/]+)(\/(?<version>[^\/]+))?/.match(path)
+
+    raise 'Unrecognised package format. Please specify as username/packagename[/version]' unless match
+
+    match
+  end
+
+  def validate_dependencies
+    # Check that each listed dependency is a valid package path and points to an actual package.
+    # Note: this will not prevent a dependency from being deleted, leaving us invalid, so clients can't assume that
+    # every dependency listed here still exists (although in general deleting something that's already been published
+    # is poor form).
+    dependencies.each do |dep|
+      begin
+        if Package.from_package_path(dep).nil?
+          errors.add(:dependencies, "Dependency '#{dep}' cannot be found")
+        end
+      rescue
+          errors.add(:dependencies, "Dependency '#{dep}' could not be parsed")
+      end
+    end
   end
 end
