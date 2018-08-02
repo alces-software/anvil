b38c7184959ae22b55468e5d1eb18ff778730930
william <william.mccumstie@outlook.com>
2018-08-02 11:26:49 +0100

    Update the postgres install script to use the tarball
    A pre-compiled tarball of postgres has been created and stored on S3.
    It can be used instead of compiling postgres from scratch.
    
    Also, the postgres data is now stored in `/var/postgres`
    
    NOTE: It is a self extracting tarball and thus only needs to be ran

--- scripts/setup-postgres.sh
+++ scripts/setup-postgres.sh
@@ -2,35 +2,41 @@
 set -e
 
 # Installs the required packages
-yum install wget readline-devel zlib-devel -y
+yum install wget -y
 
-# Moves to the build dir
-dir=/tmp/postgres-build
-mkdir -p $dir
-cd $dir
-
+#
+# The commented code was used to create the tarball, It is being kept as a
+# future reference
+#
 # Fetches the postgres source code
-postgres='postgresql-9.6.9'
-wget https://ftp.postgresql.org/pub/source/v9.6.9/$postgres.tar.gz
-tar -xzf $postgres.tar.gz
-cd $postgres
-
+# postgres='postgresql-9.6.9'
+# wget https://ftp.postgresql.org/pub/source/v9.6.9/$postgres.tar.gz
+# tar -xzf $postgres.tar.gz
+# cd $postgres
+#
 # Configures and installs postgres
-./configure --prefix=/usr
-make world -j $(nproc)
-make install-world -j $(nproc)
+# ./configure --prefix=/usr
+# make world -j $(nproc)
+# make install-world -j $(nproc)
 
-# Creates the postgres and log
-postgres_dir=/usr/share/postgresql
-log=/var/log/postgresql.log
+# Extracts the compiled version of postgres
+cd /tmp
+binary=extract-postgres.sh
+wget https://s3-eu-west-1.amazonaws.com/flight-direct/$binary
+bash $binary
+
+# Creates the postgres user
 user=postgres
 adduser $user
+
+# Creates the data and log files/dirs
+var_dir=/var/postgres
+log=/var/log/postgresql.log
+mkdir $var_dir
 touch $log
-chown -R $user $log
-mkdir -p $postgres_dir
-chown -R $user $postgres_dir
+chown -R $user $var_dir $log
 
 # Creates the db and starts it
-sudo -u $user initdb -D $postgres_dir/postgres/data
-sudo -u $user pg_ctl -D $postgres_dir/postgres/data -l $log start
+sudo -u $user initdb -D $var_dir/data
+sudo -u $user pg_ctl -D $var_dir/data -l $log start
 
