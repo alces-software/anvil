alces-software/anvil

commit 0252711943650651552fa367c9de88060c3350d0

Hack the dependency resolution during the snapshot
Anvil resolves dependencies when they are uploaded for the first time.
It does not allow a package to be added if it dependencies do not exist.

This is fine for the upload as the "developers" are, in essence, sorting
the packages. However this proves problematic for the snapshot which
is done alpha-numerically.

The snapshot does not import the packages through the same mechanism as
the upload, so the behaviour can vary. Previously it was skipping the
dependencies entirely on import. This meant the `forge-cli` client
(within Flight Direct) could not resolve dependencies. This bug has been
fixed in the Packages model.

However this also means the packages need to be imported in reverse
dependency order. The import has been hacked to repeat the import until
all the packages have been added. This retry mechanism assumes all
errors are due to missing dependencies and thus is may cause issues in
the future.

However it works for now

app/models/package.rb
@@ -5,7 +5,9 @@ class Package < ApplicationRecord
   class << self
     def build_from_zip(file:, **args)
       a = extract_metadata(file).attributes
-      new(name: a.name, version: a.version, **args)
+      new(name: a.name, version: a.version, **args).tap do |p|
+        set_attributes(p, a)
+      end
     end
 
     private
@@ -16,6 +18,15 @@ class Package < ApplicationRecord
         zip.read(zip.get_entry('metadata.json')),
         object_class: OpenStruct
       )
+    end
+
+    def set_attributes(package, attrs)
+      attrs.to_h.each do |key, value|
+        setter = "#{key.to_s.underscore}=".to_sym
+        if package.respond_to?(setter)
+          package.send(setter, value)
+        end
+      end
     end
   end
 

Gemfile
@@ -31,6 +31,7 @@ group :development, :test do
   # Use the `pry-byebug` extension instead of vanilla byebug
   gem 'pry'
   gem 'pry-byebug'
+  gem 'pry-rails'
 end
 
 group :development do

lib/tasks/packages.rake
@@ -28,13 +28,11 @@ namespace :packages do
   task import: :environment do
     raise 'The ANVIL_LOCAL_DIR has not been set' unless ENV['ANVIL_LOCAL_DIR']
     raise 'The ANVIL_BASE_URL has not been set' unless ENV['ANVIL_BASE_URL']
-    user = User.where(name: 'alces').first_or_create
-    category = Category.where(name: 'uncategorised').first_or_create
-    Dir[package_path('**/*.zip')].each do |zip_path|
-      url = extract_package_url(zip_path)
-      Package.build_from_zip(
-        user: user, category: category, package_url: url, file: zip_path
-      ).save!
+    files = Dir[package_path('**/*.zip')]
+    files.each do |path|
+      valid = add_package_from_zip_path(path)
+      next if valid
+      files << path
     end
   end
 
@@ -75,5 +73,14 @@ namespace :packages do
   rescue ActiveRecord::NoDatabaseError
     # The database doesn't exist so do nothing
     return
+  end
+
+  def add_package_from_zip_path(zip_path)
+    user = User.where(name: 'alces').first_or_create
+    category = Category.where(name: 'uncategorised').first_or_create
+    url = extract_package_url(zip_path)
+    Package.build_from_zip(
+      user: user, category: category, package_url: url, file: zip_path
+    ).save
   end
 end

commit 75c98a26c47860af267c7e035be9e6b188f706ee

Fix bug that ignored the updated package_url
The bug was introduced in:
https://github.com/alces-software/anvil/commit/0252711943650651552fa367c9de88060c3350d0

The bug was created by there being two sources for attributes (the
inputs and the metafile). The code meant the metafile attributes would
win and thus skipped updating the URL.

The fix is to merge the attributes together first.

app/models/package.rb
@@ -3,10 +3,11 @@ require 'zip'
 
 class Package < ApplicationRecord
   class << self
-    def build_from_zip(file:, **args)
-      a = extract_metadata(file).attributes
-      new(name: a.name, version: a.version, **args).tap do |p|
-        set_attributes(p, a)
+    def build_from_zip(file:, **input_args)
+      default_args = extract_metadata(file).attributes.to_h.symbolize_keys
+      args = OpenStruct.new(default_args.merge(input_args))
+      new(name: args.name, version: args.version).tap do |p|
+        set_attributes(p, args)
       end
     end
 