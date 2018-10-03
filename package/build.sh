bfda44f1bd51fd2cda158272eeb0b7c4b21d5cc5
william <william.mccumstie@outlook.com>
2018-10-03 11:32:11 +0100

    Add a forge package to the anvil repo
    These are the base files required to turn the repo into a forge package.
    
    It currently doesn't work

---
+++ package/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+package_name='anvil'
+
+if [ -f ./${package_name}.zip ]; then
+  echo "Replacing existing ${package_name}.zip in this directory"
+  rm ./${package_name}.zip
+fi
+
+temp_dir=$(mktemp -d /tmp/${package_name}-build-XXXXX)
+
+pushd "${temp_dir}" > /dev/null
+zip -r ${package_name}.zip *
+popd > /dev/null
+
+mv "${temp_dir}"/${package_name}.zip .
+
+rm -rf "${temp_dir}"
